36de40bd3b7195b131ea5d8888a56d846ef7d749
Jayanth Ilamoni <jayanthilamoni@yahoo.in>
2017-11-25 18:48:48 +0530

    Save using mongoose

--- server/server.js
+++ server/server.js
@@ -1,2 +1,25 @@
 const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/Todos');
+
+var Todo = mongoose.model('Todo',{
+  note:{
+    type:'String'
+  },completed:{
+    type:'Boolean'
+  },completedAt:{
+    type:'Number'
+  }
+});
+
+var newTodo = new Todo({
+  note: 'Buy soap',
+  completed: false
+});
+
+newTodo.save().then((result) => {
+  console.log('Sucess :',result);
+},(err) => {
+  console.log('Unable to save',err);
+});
