49b213b838c8d7fb761fcec82d60c953c70c1ee5
Jayanth Ilamoni <jayanthilamoni@yahoo.in>
2017-11-25 16:15:10 +0530

    Added delete function

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -14,11 +14,17 @@ MongoClient.connect('mongodb://localhost:27017/Todos', (err, db) => {
   //   console.log('Unable to fetch values');
   // });
 
-  db.collection('users').find({age: 23}).toArray().then((docs) => {
-    console.log(JSON.stringify(docs,undefined,2));
+  // db.collection('users').find({age: 23}).toArray().then((docs) => {
+  //   console.log(JSON.stringify(docs,undefined,2));
+  // },(err) => {
+  //   console.log('Unable to fetch values');
+  // });
+
+  db.collection('TodoApp').findOneAndDelete({_id:new ObjectID("5a1946c15767a6b767969ccf")}).then((result) => {
+    console.log(JSON.stringify(result,undefined,2));
   },(err) => {
     console.log('Unable to fetch values');
   });
 
-  db.close();
+  //db.close();
 });
